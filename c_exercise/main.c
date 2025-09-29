e6a852252ea6acadf0a49657212daddfded8d1cf
Yuki Nakashiba <nakashibay@whitman.edu>
2025-09-29 13:24:57 -0700

    made lines to make it easier to see

--- c_exercise/main.c
+++ c_exercise/main.c
@@ -10,6 +10,7 @@ int main()
     printf("Enter 3 components of the first vector\n");
     scanf("%d %d %d", &v1[0], &v1[1], &v1[2]);
 
+    printf("---------------------------------------\n");
     printf("Enter 3 components of the second vector\n");
     scanf("%d %d %d", &v2[0], &v2[1], &v2[2]);
 
@@ -20,6 +21,7 @@ int main()
         dot_product += v1[i] * v2[i];
         i++;
     }
+    printf("---------------------------------------\n");
     printf("Dot product is: %i\n", dot_product);
     return (0);
 }
